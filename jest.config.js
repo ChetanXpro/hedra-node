f4e648065e929b6178e8b3da7f5793ba2b849011
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-10-16 21:55:57 +0530

    Fix jest test config to use correct test folder

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
-    testMatch: [
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: [
       '**/__tests__/**/*.+(ts|tsx|js)',
       '**/?(*.)+(spec|test).+(ts|tsx|js)'
-    ],
-    transform: {
+  ],
+  transform: {
       '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
+  },
 };
